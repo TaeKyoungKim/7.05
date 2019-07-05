af0f17174813921c8c9403a95837e37281f4cd3c
Your Name <you@example.com>
2019-07-05 18:01:01 +0900

    first Commit

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [  {  
+         'id': 1,  
+         'title':'Article one',  
+         'body':"Found a bug? Have a good idea for improving Flask? Head over to Flask's github page and create a new ticket or fork. If you just want to chat with fellow developers, visit the IRC channel or join the mailinglist. You can also directly add issues and feature requests to the issue tracker. Fork me on GitHub",  
+         'author':'GaryKIm',  
+         'create_date':'04-09-2018',  },
+
+        {  'id': 2,  
+        'title':'Article two',  
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+        'author':'vasanth nagarajan',  
+        'create_date':'05-09-2018',  },  
+        {   'id': 3,  
+            'title':'Article three',  
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            'author':'nagarajan vasanth',  
+            'create_date':'04-09-2018',  } ] 
+    return articles 
